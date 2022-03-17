87743ff66030aafd9bdad90a3241c4886021dd33
Cliffano Subagio <cliffano@gmail.com>
2022-03-17 21:27:42 +1100

    Access gallery bucket as group now.

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_s3_bucket" "gallery" {
 
 resource "aws_s3_bucket_public_access_block" "gallery_private" {
 
-    bucket = aws_s3_bucket.gallery.id
+    bucket = aws_s3_bucket.gallery[0].id
 
     block_public_acls       = true
     block_public_policy     = true
