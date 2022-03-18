d78735a18327148108c5fb869cbf6b963085ba4e
Cliffano Subagio <cliffano@gmail.com>
2022-03-19 00:46:01 +1100

    Remove unused aliases var. Add description to all variables.

--- variables.tf
+++ variables.tf
@@ -1,66 +1,53 @@
+variable "region" {
+  type        = string
+  default     = "ap-southeast-2"
+  description = "AWS region https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html"
+}
+
 variable "acm_certificate_arn" {
   type        = string
   default     = null
-  description = ""
+  description = "ARN of SSL certificate in ACM"
 }
 
 variable "s3_bucket_cdn_log" {
   type        = string
   default     = null
-  description = ""
+  description = "Name of S3 bucket for storing CDN log"
 }
 
 variable "s3_bucket_site" {
   type        = string
   default     = null
-  description = ""
-}
-
-variable "s3_bucket_gallery" {
-  type        = string
-  default     = null
-  description = ""
+  description = "Name of S3 bucket for storing web site content"
 }
 
 variable "enable_s3_bucket_gallery" {
   type        = bool
   default     = false
-  description = ""
+  description = "Enable the provisioning of S3 bucket for gallery images"
 }
 
-variable "lambda_origin_request_arn" {
+variable "s3_bucket_gallery" {
   type        = string
   default     = null
-  description = ""
-}
-
-variable "region" {
-  type        = string
-  default     = null
-  description = ""
+  description = "Name of S3 bucket for storing gallery images"
 }
 
 variable "route53_domain_name" {
   type        = string
   default     = null
-  description = ""
+  description = "Route53 record to be used as the domain name of the web site"
 }
 
 variable "route53_domain_zoneid" {
   type        = string
   default     = null
-  description = ""
+  description = "Route53 hosted zone where the domain record will be added to"
 }
 
-variable "route53_domain_alias_name" {
+variable "lambda_origin_request_arn" {
   type        = string
   default     = null
-  description = ""
+  description = "ARN of Lambda Function to be used as origin-request CloudFront Lambda@Edge"
 }
-
-variable "route53_domain_alias_zoneid" {
-  type        = string
-  default     = null
-  description = ""
-}
-
