4dd70f4e8c1ecd27bb6b2d9a168416d7d148c787
Cliffano Subagio <cliffano@gmail.com>
2022-04-12 21:05:18 +1000

    Add cdn zone ID as output.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output  "cloudfront_distribution_cdn_hosted_zone_id" {
+  value = aws_cloudfront_distribution.cdn.hosted_zone_id
+}
+
 output "s3_bucket_site_arn" {
   value = aws_s3_bucket.site.arn
 }
