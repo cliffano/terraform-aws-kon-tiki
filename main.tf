f998e5544543d9e0c799c531d18bbc91e6979036
Cliffano Subagio <cliffano@gmail.com>
2022-09-11 17:38:27 +1000

    Reinstate route53 record.

--- main.tf
+++ main.tf
@@ -153,3 +153,15 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   tags = var.tags
 }
+
+resource "aws_route53_record" "domain" {
+  name    = var.route53_domain
+  zone_id = var.route53_zone_id
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
