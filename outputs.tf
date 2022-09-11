42f6709fa1812d561fd58135b0300c537d541ce9
Cliffano Subagio <cliffano@gmail.com>
2022-09-11 17:38:48 +1000

    Update output list with additional resources.

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,22 @@ output "s3_bucket_site_website_domain" {
   value = aws_s3_bucket.site.website_domain
 }
 
+output  "cloudfront_distribution_cdn_arn" {
+  value = aws_cloudfront_distribution.cdn.arn
+}
+
 output  "cloudfront_distribution_cdn_id" {
   value = aws_cloudfront_distribution.cdn.id
 }
 
 output  "cloudfront_distribution_cdn_hosted_zone_id" {
   value = aws_cloudfront_distribution.cdn.hosted_zone_id
+}
+
+output  "route53_record_domain_arn" {
+  value = aws_route53_record.domain.arn
+}
+
+output  "route53_record_domain_zone_id" {
+  value = aws_route53_record.domain.zone_id
 }
