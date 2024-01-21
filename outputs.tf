d0ff73d286f931ae21bb7cdc09dc459436969041
Cliffano Subagio <cliffano@gmail.com>
2024-01-21 12:50:25 +1100

    Add description for output resources.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "s3_bucket_site" {
-  value = aws_s3_bucket.site
+  value       = aws_s3_bucket.site
+  description = "Provisioned S3 bucket resource"
 }
 
 output  "cloudfront_distribution_cdn" {
-  value = aws_cloudfront_distribution.cdn
+  value       = aws_cloudfront_distribution.cdn
+  description = "Provisioned CloudFront distribution resource"
 }
 
 output  "route53_record_domain" {
-  value = aws_route53_record.domain
+  value       = aws_route53_record.domain
+  description = "Provisioned Route53 record resource"
 }
